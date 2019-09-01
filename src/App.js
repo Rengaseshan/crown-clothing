1cafee208387ba128113a6b19f60a03ab87560ab
rengaseshan <v.r.seshan@gmail.com>
2019-09-02 02:55:15 +0530

    added basic router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import {Route, Switch,Link} from 'react-router-dom';
 import './App.css';
+
+const HatsPage=() =>(
+  <div>
+      <h1>
+       Hats Page Works !!!
+      </h1>
+   </div>
+);
+
 
 function App() {
   return (
-    <div>
-      <HomePage></HomePage>
+    <div>  
+            <Route exact path='/' component={HomePage}></Route>
+            <Route exact path='/hats' component={HatsPage}></Route>
     </div>
   );
 }
